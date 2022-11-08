3c39bb14751695a6cac3ab9d21b387f126b709ad
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-11-08 13:19:02 +0100

    Update e2e_test.js
    two scenarios for invalid username/password added

--- e2e_test.js
+++ e2e_test.js
@@ -1,9 +1,29 @@
 Feature("Zero bank app - E2E Tests")
 
-Scenario("Login test - Negative", ({ I }) => {
+Scenario("Login test - Negative: wrong password and username", ({ I }) => {
     I.amOnPage("http://zero.webappsecurity.com/index.html")
     I.click('#signin_button')  // click using element's ID
     I.fillField('#user_login', 'invalid_username')  
+    I.fillField('#user_password', 'invalid_password')
+    I.click('Sign in')  // click using element's text
+    I.seeElement('.alert-error')
+    I.see('Login and/or password are wrong.')  // assert by displayed text
+}) 
+
+Scenario("Login test - Negative: wrong username", ({ I }) => {
+    I.amOnPage("http://zero.webappsecurity.com/index.html")
+    I.click('#signin_button')  // click using element's ID
+    I.fillField('#user_login', 'invalid_username')  
+    I.fillField('#user_password', 'password')
+    I.click('Sign in')  // click using element's text
+    I.seeElement('.alert-error')
+    I.see('Login and/or password are wrong.')  // assert by displayed text
+}) 
+
+Scenario("Login test - Negative: wrong password", ({ I }) => {
+    I.amOnPage("http://zero.webappsecurity.com/index.html")
+    I.click('#signin_button')  // click using element's ID
+    I.fillField('#user_login', 'username')  
     I.fillField('#user_password', 'invalid_password')
     I.click('Sign in')  // click using element's text
     I.seeElement('.alert-error')
