dfb90369fa2ba941e1f3a37e8985f4bd72f703ac
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-11-08 12:50:19 +0100

    Update example_test.js
    multiple scenarios example added

--- example_test.js
+++ example_test.js
@@ -1,9 +1,21 @@
-Feature('example');
+Feature('My Test Suite');
+
+// tests are running from top to bottom one by one
+// scenarios are same, just a demo:
 
 Scenario('Should load example.com website', ({ I }) => {
     I.amOnPage("https://www.example.com")
     I.see('Example')  // example is located on example page
-    I.dontSee('Google')  // google text is not located on example page
     I.seeElement("h1")  // specific element is located on example page
+});
+
+Scenario('Should load example.com website - second test', ({ I }) => {
+    I.amOnPage("https://www.example.com")
+    I.dontSee('Google')  // google text is not located on example page
     I.dontSeeElement("#not_existing_element")  // specific element is not located on example page
+});
+
+Scenario('Should load example.com website - third test', ({ I }) => {
+    I.amOnPage("https://www.example.com")
+    I.seeInCurrentUrl("example.com")  // part of the yrl check
 });
