fc010ea5386b4d51d98be1fe2d2bdd120b0ac01e
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-11-08 13:17:35 +0100

    positive login scenario added

--- e2e_test.js
+++ e2e_test.js
@@ -9,3 +9,14 @@ Scenario("Login test - Negative", ({ I }) => {
     I.seeElement('.alert-error')
     I.see('Login and/or password are wrong.')  // assert by displayed text
 }) 
+
+Scenario("Login test - Positive", ({ I }) => {
+    I.amOnPage("http://zero.webappsecurity.com/index.html")
+    I.click('#signin_button')  // click using element's ID
+    I.fillField('#user_login', 'username')  
+    I.fillField('#user_password', 'password')
+    I.click('Sign in')  // click using element's text
+    // there is page problem, getting error, so using this trick with redirect:
+    I.amOnPage('http://zero.webappsecurity.com/online-banking.html')
+    I.see('Our Bank is trusted by over 1,000,000 customers world wide.')
+}) 
