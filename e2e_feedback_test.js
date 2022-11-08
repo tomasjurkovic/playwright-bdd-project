79d085def9045b8829b131f51a3e4f9e18c74ea6
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-11-08 13:42:32 +0100

    e2e for feedback part is ready

---
+++ e2e_feedback_test.js
@@ -0,0 +1,25 @@
+Feature('Zero Bank Application - E2E Feedback tests')
+
+Scenario('Feedback form - Reset form', ({ I }) => {
+    I.amOnPage('http://zero.webappsecurity.com/feedback.html')
+    I.fillField('#name', 'Tomas')
+    I.fillField('#email', 'tomas@jurkovic.com')
+    I.fillField('#subject', 'My feedback')
+    I.fillField('#comment', 'Learning codecept')
+    I.click("input[value='Clear']")
+    I.seeInField('#name', '')
+    I.seeInField('#email', '')
+    I.seeInField('#subject', '')
+    I.seeInField('#comment', '')
+})
+
+Scenario('Feedback form - Submit form', ({ I }) => {
+    I.amOnPage('http://zero.webappsecurity.com/feedback.html')
+    I.fillField('#name', 'Tomas')
+    I.fillField('#email', 'tomas@jurkovic.com')
+    I.fillField('#subject', 'My feedback')
+    I.fillField('#comment', 'Learning codecept')
+    I.click("input[value='Send Message']")
+    I.seeElement(".offset3")
+    I.see("Thank you for your comments, Tomas. They will be reviewed by our Customer Service staff and given the full attention that they deserve.")
+})
